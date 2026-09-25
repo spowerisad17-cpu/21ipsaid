4898313076a7a950517110af693e2df8fc316991
spowerisad17-cpu <spowerisad17@gmail.com>
2026-09-25 12:22:41 +0300

    Delete Untitled-1.py

--- Untitled-1.py
+++
@@ -1,8 +0,0 @@
-shopping_list = ["хлеб", "молоко", "сыр", "яблоки"]
-#создаем список из 4 продуктов
-shopping_list.append("шоколад")
-#выводим список
-print("Список покупок:", shopping_list)
-#Создаем множество с теми же продуктами
-unique_items = set(shopping_list)
-print("Множества:", unique_items)
